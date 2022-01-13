09419520897c0a441e1101a59898c82547930214
Ganesh Shrestha <37283650+shresthaganesh@users.noreply.github.com>
2022-01-13 22:42:47 +1100

    Update armscript.ps1

--- armscript.ps1
+++ armscript.ps1
@@ -1,12 +1,15 @@
 param([string] $name)
 $output = 'Hello {0}' -f $name
 Write-Output $output
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $output
 
+$a = '::before::'
 Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Scope AllUsers -Force
 Install-Module -Name PowerShellGet -Force -AllowClobber
 Install-Module -Name MicrosoftTeams -RequiredVersion 2.5.0 -Force -AllowClobber
 Install-Module -Name MSOnline -RequiredVersion 1.1.183.57 -Scope AllUsers -Force
 Install-Module -Name Posh-SSH -RequiredVersion 2.3.0 -Scope AllUsers -Force
 
+$a+="after"
+
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output+$a
